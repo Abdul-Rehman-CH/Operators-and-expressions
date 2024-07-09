82512967823ab8320d2501a677404fe83834a110
Abdul-Rehman-CH <Abdulrehmanc435@gmail.com>
2024-07-09 14:56:23 +0500

    JS operators

---
+++ script.js
@@ -0,0 +1,51 @@
+console.log("Operators in Java script")
+// Arithmetic Operators
+let a = 20;
+let b = 10;
+console.log("a + b =", a+b);
+console.log("a - b =", a-b);
+console.log("a * b =", a*b);
+console.log("a / b =", a/b);
+console.log("a % b =", a&b);
+console.log("a ** b =", a**b)
+console.log("a ++ =", a++);
+console.log("a -- =", a--);
+console.log("--a =", --a);
+console.log("++a =", ++a);
+// Assignment Operators
+let x = 10
+x += 5
+console.log("X after += is",x);
+x -= 5
+console.log("X after -= is",x);
+x *= 5
+console.log("X after *= is",x);
+// To end confusion replace x with y in this because this value consider x*=5 as the value 
+
+let y= 10
+y /= 5
+console.log("Y after /= is",y);
+
+// Comparison Operators
+let comp1 = 10
+let comp2 = 20
+console.log("comp1 == comp2 =", comp1 == comp2);
+console.log("comp1 != comp2 =", comp1 != comp2);
+//Now if we were given a number in string so == will return true but === will return false
+let Comparison1 = 10
+let Comparison2 = "10"
+console.log("Comaparison1 ==  Comparison2 =", Comparison1==Comparison2)
+console.log("Comaparison1 ===  Comparison2 =", Comparison1===Comparison2)
+console.log("Comaparison1 !==  Comparison2 =", Comparison1!==Comparison2)
+
+//Logical Operators
+let a1 = 10
+let b1 = 20
+console.log("a1 =",a1);
+console.log("b1 =",b1);
+console.log("a1<b1 && b1==a1 =", a1<b1 && b1==a1);
+console.log("a1<b1 || b1==a1 =", a1<b1 || b1==a1);
+
+
+
+
